2e61bfe3822d64dbda003307c8e9d52b6e1c0147
Colin <moshen.colin@gmail.com>
2012-01-13 00:24:28 -0600

    Added writeANSI(2) to qrenc.
    Writes out ANSI color coded QR codes!

--- qrenc.c
+++ qrenc.c
@@ -330,6 +330,108 @@ static int writeEPS(QRcode *qrcode, const char *outfile)
 	return 0;
 }
 
+static int writeANSI(QRcode *qrcode, const char *outfile)
+{
+	FILE *fp;
+	unsigned char *row, *p;
+	int x, y, yy;
+	int realwidth;
+	int last;
+
+	char *white, *black, *buffer;
+	int white_s, black_s, buffer_s;
+
+	if( image_type == ANSI256_TYPE ){
+		/* codes for 256 color compatible terminals */
+		white = "\033[48;5;231m";
+		white_s = 11;
+		black = "\033[48;5;16m";
+		black_s = 10;
+	} else {
+		white = "\033[47m";
+		white_s = 5;
+		black = "\033[40m";
+		black_s = 5;
+	}
+
+	size = 1;
+	margin = 1;
+
+	if(outfile[0] == '-' && outfile[1] == '\0') {
+		fp = stdout;
+	} else {
+		fp = fopen(outfile, "wb");
+		if(fp == NULL) {
+			fprintf(stderr, "Failed to create file: %s\n", outfile);
+			perror(NULL);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	realwidth = (qrcode->width + margin * 2) * size;
+	buffer_s = ( realwidth * white_s ) * 2;
+	buffer = (char *)malloc( buffer_s );
+	if(buffer == NULL) {
+		fprintf(stderr, "Failed to allocate memory.\n");
+		exit(EXIT_FAILURE);
+	}
+	bzero( buffer, buffer_s );
+
+	/* top margin */
+	strncpy(buffer, white, white_s);
+	for(y=0; y<realwidth; y++){
+		strncat(buffer, "  ", 2);
+	}
+	strncat(buffer, "\n", 1);
+	fputs(buffer, fp);
+
+	/* data */
+	p = qrcode->data;
+	for(y=0; y<qrcode->width; y++) {
+		row = (p+(y*qrcode->width));
+		yy = (margin + qrcode->width - y - 1);
+
+		bzero( buffer, buffer_s );
+		strncpy( buffer, white, white_s );
+		strncat( buffer, "  ", 2 );
+		last = 0;
+
+		for(x=0; x<qrcode->width; x++) {
+			if(*(row+x)&0x1) {
+				if( last != 1 ){
+					strncat( buffer, black, black_s );
+					last = 1;
+				}
+			} else {
+				if( last != 0 ){
+					strncat( buffer, white, white_s );
+					last = 0;
+				}
+			}
+			strncat( buffer, "  ", 2 );
+		}
+
+		if( last != 0 ){
+			strncat( buffer, white, white_s );
+		}
+		strncat( buffer, "  \n", 3 );
+		fputs( buffer, fp );
+	}
+
+	/* bottom margin */
+	bzero( buffer, buffer_s);
+	strncpy(buffer, white, white_s);
+	for(y=0; y<realwidth; y++){
+		strncat(buffer, "  ", 2);
+	}
+	strncat(buffer, "\033[0m\n", 5); // reset to default colors
+	fputs(buffer, fp);
+	fclose(fp);
+	free(buffer);
+
+	return 0;
+}
+
 static QRcode *encode(const unsigned char *intext, int length)
 {
 	QRcode *code;
