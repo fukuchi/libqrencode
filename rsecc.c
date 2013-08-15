742c4790a2a57e897443ebc8492c69e92d6b0a99
Kentaro Fukuchi <kentaro@fukuchi.org>
2013-08-15 17:02:18 +0900

    Minor bug fix.

--- rsecc.c
+++ rsecc.c
@@ -66,7 +66,7 @@ static void RSECC_initLookupTable(void)
 void RSECC_init(void)
 {
 	RSECC_initLookupTable();
-	memset(generatorInitialized, 0, (max_length - min_length));
+	memset(generatorInitialized, 0, (max_length - min_length + 1));
 	initialized = 1;
 }
 
