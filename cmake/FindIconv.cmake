45e594d5cd632a9c639cecd53def2eba656194ab
Aleksey Nikolaev <aleksey.nikolaev@opensoftdev.ru>
2017-01-17 17:31:41 +0300

    Reverted the BUILD_TESTING option and marked as deprecated. Rewritten the FindIconv.cmake.

--- cmake/FindIconv.cmake
+++ cmake/FindIconv.cmake
@@ -1,145 +1,112 @@
-# vim:ts=4:sw=4:expandtab:autoindent:
-#
-# The MIT License
-#
-# Copyright (c) 2008, 2009 Flusspferd contributors (see "CONTRIBUTORS" or
-#                                      http://flusspferd.org/contributors.txt)
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-
 include(CheckFunctionExists)
-include(CheckCSourceCompiles)
 
-if(ICONV_INCLUDE_DIR)
-  set(ICONV_FIND_QUIETLY TRUE)
+set(_ICONV_SEARCHES)
+
+# Search ICONV_DIR first if it is set.
+if(NOT ICONV_DIR AND ENV{ICONV_DIR})
+  set(ICONV_DIR $ENV{ICONV_DIR})
 endif()
 
-find_path(ICONV_INCLUDE_DIR iconv.h
-    HINTS
-    ${CMAKE_PREFIX_PATH}
-    ${ICONV_DIR}
-    $ENV{ICONV_DIR}
-    PATH_SUFFIXES include
-)
-
-if(NOT ICONV_INCLUDE_DIR STREQUAL "ICONV_INCLUDE_DIR-NOTFOUND")
-    set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
-    check_function_exists(iconv_open ICONV_IN_GLIBC)
+if(ICONV_DIR)
+  set(_ICONV_DIR_SEARCH PATHS ${ICONV_DIR} NO_DEFAULT_PATH)
+  list(APPEND _ICONV_SEARCHES _ICONV_DIR_SEARCH)
 endif()
 
-if(NOT ICONV_IN_GLIBC)
-    if (CMAKE_CL_64)
-        find_library(ICONV_LIBRARY
-            NAMES iconv64
-            HINTS
-            ${CMAKE_PREFIX_PATH}
-            ${ICONV_DIR}
-            $ENV{ICONV_DIR}
-            PATH_SUFFIXES lib64 lib
-            )
-    else()
-        find_library(ICONV_LIBRARY
-            NAMES iconv
-            HINTS
-            ${CMAKE_PREFIX_PATH}
-            ${ICONV_DIR}
-            $ENV{ICONV_DIR}
-            PATH_SUFFIXES lib64 lib
-            )
-    endif()
-    set(ICONV_TEST ${ICONV_LIBRARY})
-else()
-    set(ICONV_TEST "In glibc")
+# Normal search.
+set(_ICONV_SEARCH_NORMAL
+  PATHS "[HKEY_LOCAL_MACHINE\\SOFTWARE\\GnuWin32\\Iconv;InstallPath]"
+        "$ENV{PROGRAMFILES}/iconv"
+  )
+list(APPEND _ICONV_SEARCHES _ICONV_SEARCH_NORMAL)
+
+set(ICONV_NAMES iconv iconv2 libiconv iconv64)
+set(ICONV_NAMES_DEBUG iconvd iconv64d)
+
+# Try each search configuration.
+foreach(search ${_ICONV_SEARCHES})
+  find_path(ICONV_INCLUDE_DIR NAMES iconv.h ${${search}} PATH_SUFFIXES include)
+endforeach()
+
+# Allow ICONV_LIBRARY to be set manually, as the location of the iconv library
+if(NOT ICONV_LIBRARY)
+  foreach(search ${_ICONV_SEARCHES})
+    find_library(ICONV_LIBRARY_RELEASE NAMES ${ICONV_NAMES} ${${search}} PATH_SUFFIXES lib)
+    find_library(ICONV_LIBRARY_DEBUG   NAMES ${ICONV_NAMES_DEBUG} ${${search}} PATH_SUFFIXES lib)
+  endforeach()
+
+  include(SelectLibraryConfigurations)
+  select_library_configurations(ICONV)
 endif()
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ICONV DEFAULT_MSG ICONV_TEST ICONV_INCLUDE_DIR)
-
-if(ICONV_FOUND)
-    set(ICONV_INCLUDE_DIRS ${ICONV_INCLUDE_DIR})
-
-    if(NOT ICONV_LIBRARIES)
-      set(ICONV_LIBRARIES ${ICONV_LIBRARY})
-    endif()
-
-    if(NOT TARGET ICONV::ICONV)
-      add_library(ICONV::ICONV UNKNOWN IMPORTED)
-      set_target_properties(ICONV::ICONV PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${ICONV_INCLUDE_DIRS}")
-
-      if(ICONV_LIBRARY_RELEASE)
-        set_property(TARGET ICONV::ICONV APPEND PROPERTY
-          IMPORTED_CONFIGURATIONS RELEASE)
-        set_target_properties(ICONV::ICONV PROPERTIES
-          IMPORTED_LOCATION_RELEASE "${ICONV_LIBRARY_RELEASE}")
-      endif()
-
-      if(ICONV_LIBRARY_DEBUG)
-        set_property(TARGET ICONV::ICONV APPEND PROPERTY
-          IMPORTED_CONFIGURATIONS DEBUG)
-        set_target_properties(ICONV::ICONV PROPERTIES
-          IMPORTED_LOCATION_DEBUG "${ICONV_LIBRARY_DEBUG}")
-      endif()
-
-      if(NOT ICONV_LIBRARY_RELEASE AND NOT ICONV_LIBRARY_DEBUG)
-        set_property(TARGET ICONV::ICONV APPEND PROPERTY
-          IMPORTED_LOCATION "${ICONV_LIBRARY}")
-      endif()
-    endif()
-endif()
-
-#if(ICONV_FOUND)
-#    set(CMAKE_REQUIRED_INCLUDES ${ICONV_INCLUDE_DIR})
-#    set(CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARIES})
-#
-#    if (NOT DEFINED ICONV_ACCEPTS_NONCONST_INPUT)
-#        # Display a useful message first time we come through here
-#        message(STATUS "One (and only one) of the ICONV_ACCEPTS_... tests must pass")
-#    endif()
-#
-#    check_c_source_compiles(
-#        "#include <iconv.h>
-#         int main() {
-#            char *p = 0;
-#            iconv(iconv_t(-1), &p, 0, 0, 0);
-#         }"
-#        ICONV_ACCEPTS_NONCONST_INPUT)
-#
-#    check_c_source_compiles(
-#        "#include <iconv.h>
-#         int main() {
-#            char const *p = 0;
-#            iconv(iconv_t(-1), &p, 0, 0, 0);
-#         }"
-#        ICONV_ACCEPTS_CONST_INPUT)
-#
-#    if (ICONV_LIBRARY)
-#        list(REMOVE_ITEM CMAKE_REQUIRED_LIBRARIES ${ICONV_LIBRARY})
-#        list(REMOVE_DUPLICATES CMAKE_REQUIRED_LIBRARIES)
-#    endif()
-#
-#    if(NOT ICONV_ACCEPTS_CONST_INPUT AND NOT ICONV_ACCEPTS_NONCONST_INPUT)
-#      MESSAGE(FATAL_ERROR "Unable to determine iconv() signature")
-#    elseif(ICONV_ACCEPTS_CONST_INPUT AND ICONV_ACCEPTS_NONCONST_INPUT)
-#      MESSAGE(FATAL_ERROR "Unable to determine iconv() signature - both test cases passed!")
-#    endif()
-#endif()
+unset(ICONV_NAMES)
+unset(ICONV_NAMES_DEBUG)
 
 mark_as_advanced(ICONV_LIBRARY ICONV_INCLUDE_DIR)
+
+if(ICONV_INCLUDE_DIR AND EXISTS "${ICONV_INCLUDE_DIR}/iconv.h")
+    file(STRINGS "${ICONV_INCLUDE_DIR}/iconv.h" ICONV_H REGEX "^#define _LIBICONV_VERSION 0x([0-9]+)")
+
+    string(REGEX MATCH "#define _LIBICONV_VERSION 0x([0-9][0-9])([0-9][0-9])?([0-9][0-9])?.*" temp_match "${ICONV_H}")
+    unset(temp_match)
+    set(ICONV_VERSION_MAJOR "${CMAKE_MATCH_1}")
+    set(ICONV_VERSION_MINOR "${CMAKE_MATCH_2}")
+    set(ICONV_VERSION_PATCH "${CMAKE_MATCH_3}")
+    string(REGEX REPLACE "0*([1-9][0-9]*).*" "\\1" ICONV_VERSION_MAJOR "${ICONV_VERSION_MAJOR}")
+    string(REGEX REPLACE "0*([1-9][0-9]*).*" "\\1" ICONV_VERSION_MINOR "${ICONV_VERSION_MINOR}")
+    string(REGEX REPLACE "0*([1-9][0-9]*).*" "\\1" ICONV_VERSION_PATCH "${ICONV_VERSION_PATCH}")
+    
+    set(ICONV_VERSION_STRING "${ICONV_VERSION_MAJOR}.${ICONV_VERSION_MINOR}")
+    if(ICONV_VERSION_PATCH)
+        set(ICONV_VERSION_STRING "${ICONV_VERSION_STRING}.${ICONV_VERSION_PATCH}")
+    endif()
+endif()
+
+check_function_exists(iconv_open ICONV_IN_GLIBC)
+
+if(ICONV_IN_GLIBC)
+    set(ICONV_TEST "In glibc")
+else()
+    set(ICONV_TEST ${ICONV_LIBRARY})
+endif()
+
+# handle the QUIETLY and REQUIRED arguments and set ICONV_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ICONV
+    REQUIRED_VARS ICONV_LIBRARY ICONV_INCLUDE_DIR
+    VERSION_VAR ICONV_VERSION_STRING)
+
+if(NOT ICONV_FOUND)
+    return()
+endif()
+
+set(ICONV_INCLUDE_DIRS ${ICONV_INCLUDE_DIR})
+
+if(NOT ICONV_LIBRARIES)
+    set(ICONV_LIBRARIES ${ICONV_LIBRARY})
+endif()
+
+if(NOT TARGET ICONV::ICONV)
+    add_library(ICONV::ICONV UNKNOWN IMPORTED)
+    set_target_properties(ICONV::ICONV PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${ICONV_INCLUDE_DIRS}")
+
+    if(ICONV_LIBRARY_RELEASE)
+        set_property(TARGET ICONV::ICONV APPEND PROPERTY
+            IMPORTED_CONFIGURATIONS RELEASE)
+        set_target_properties(ICONV::ICONV PROPERTIES
+            IMPORTED_LOCATION_RELEASE "${ICONV_LIBRARY_RELEASE}")
+    endif()
+
+    if(ICONV_LIBRARY_DEBUG)
+        set_property(TARGET ICONV::ICONV APPEND PROPERTY
+            IMPORTED_CONFIGURATIONS DEBUG)
+        set_target_properties(ICONV::ICONV PROPERTIES
+            IMPORTED_LOCATION_DEBUG "${ICONV_LIBRARY_DEBUG}")
+    endif()
+
+    if(NOT ICONV_LIBRARY_RELEASE AND NOT ICONV_LIBRARY_DEBUG)
+        set_property(TARGET ICONV::ICONV APPEND PROPERTY
+            IMPORTED_LOCATION "${ICONV_LIBRARY}")
+    endif()
+endif()
