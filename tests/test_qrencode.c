2e91a7d03675020ecfcab715108b0183f3982a03
Kentaro Fukuchi <kentaro@fukuchi.org>
2020-09-26 17:14:33 +0900

    Minor memory leak bug in a test case has been fixed.

--- tests/test_qrencode.c
+++ tests/test_qrencode.c
@@ -685,12 +685,24 @@ static void test_encodeTooLongMQR(void)
 	code = QRcode_encodeStringMQR(data[0], 1, QR_ECLEVEL_L, QR_MODE_8, 0);
 	assert_nonnull(code, "6 byte length numeric string should be accepted to version 2 or larger.\n");
 	assert_equal(code->version, 2, "6 byte length numeric string should be accepted to version 2.\n");
+	if(code != NULL) {
+		QRcode_free(code);
+	}
+
 	code = QRcode_encodeStringMQR(data[1], 2, QR_ECLEVEL_L, QR_MODE_8, 0);
 	assert_nonnull(code, "7 byte length alphanumeric string should be accepted to version 3 or larger.\n");
 	assert_equal(code->version, 3, "7 byte length alphanumeric string should be accepted to version 3.\n");
+	if(code != NULL) {
+		QRcode_free(code);
+	}
+
 	code = QRcode_encodeString8bitMQR(data[2], 3, QR_ECLEVEL_L);
 	assert_nonnull(code, "9 byte length 8bit string should be accepted to version 4.\n");
 	assert_equal(code->version, 4, "9 byte length 8bit string should be accepted to version 4.\n");
+	if(code != NULL) {
+		QRcode_free(code);
+	}
+
 	code = QRcode_encodeString8bitMQR(data[3], 4, QR_ECLEVEL_L);
 	assert_null(code, "16 byte length 8bit string was accepted to version 4.\n");
 	assert_equal(errno, ERANGE, "errno != ERANGE\n");
