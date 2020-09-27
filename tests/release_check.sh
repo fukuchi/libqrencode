9ea13e31582060824eb43cae9540c9a4f9073d7e
Kentaro Fukuchi <kentaro@fukuchi.org>
2020-09-28 04:40:21 +0900

    README.md check has been added.

--- tests/release_check.sh
+++ tests/release_check.sh
@@ -14,7 +14,6 @@ if [ "$Configure_version_string" != "$Cmake_version_string" ]; then
 	echo "ERROR: Version number defined in configure.ac is not equal to the number defined in CMakeLists.txt."
 	echo "configure.ac: $Configure_version_string"
 	echo "CMakeLists.txt: $Cmake_version_string"
-	exit 1
 fi
 
 NEWS_version_string=`grep -m 1 Version ../NEWS | sed 's/^Version \([^ ]\+\) (.*$/\1/'`
@@ -23,5 +22,12 @@ if [ "$Configure_version_string" != "$NEWS_version_string" ]; then
 	echo "ERROR: Version number defined in configure.ac is not equal to the newest number described in NEWS."
 	echo "configure.ac: $Configure_version_string"
 	echo "NEWS: $NEWS_version_string"
-	exit 1
+fi
+
+README_version_string=`grep -m 1 Attention ../README.md | sed 's/^.*version \([^ ]\+\)\.$/\1/'`
+
+if [ "$Configure_version_string" != "$README_version_string" ]; then
+	echo "ERROR: Version number defined in configure.ac is not equal to the newest number described in README.md."
+	echo "configure.ac: $Configure_version_string"
+	echo "README.md: $README_version_string"
 fi
