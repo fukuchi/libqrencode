fukuchi/libqrencode

commit 3e629c7ba1dd67bcfff5d43ef338be77cfd9ed23

data buffer now allocated as a static array.

qrenc.c
@@ -227,18 +227,13 @@ static int color_set(unsigned char color[4], const char *value)
 	return 0;
 }
 
-#define MAX_DATA_SIZE (7090 * 16) /* from the specification */
+#define MAX_DATA_SIZE (7090 * 2) /* timed by the safty factor 2 */
+static unsigned char data_buffer[MAX_DATA_SIZE];
 static unsigned char *readFile(FILE *fp, int *length)
 {
-	unsigned char *buffer;
 	int ret;
 
-	buffer = (unsigned char *)malloc(MAX_DATA_SIZE + 1);
-	if(buffer == NULL) {
-		fprintf(stderr, "Memory allocation failed.\n");
-		exit(EXIT_FAILURE);
-	}
-	ret = fread(buffer, 1, MAX_DATA_SIZE, fp);
+	ret = fread(data_buffer, 1, MAX_DATA_SIZE, fp);
 	if(ret == 0) {
 		fprintf(stderr, "No input data.\n");
 		exit(EXIT_FAILURE);
@@ -248,10 +243,10 @@ static unsigned char *readFile(FILE *fp, int *length)
 		exit(EXIT_FAILURE);
 	}
 
-	buffer[ret] = '\0';
+	data_buffer[ret] = '\0';
 	*length = ret;
 
-	return buffer;
+	return data_buffer;
 }
 
 static FILE *openFile(const char *outfile)

commit ec7f4af217105a835a363ac52f3fa358cbd35a34

Fix MAX_DATA_SIZE, in case 16 symbols are needed by structured appending
Fix a bug introduced in the commit 3e629c7

qrenc.c
@@ -32,6 +32,7 @@
 #endif
 
 #include "qrencode.h"
+#include "qrinput.h"
 
 #define INCHES_PER_METER (100.0/2.54)
 
@@ -237,7 +238,7 @@ static int color_set(unsigned char color[4], const char *value)
 	return 0;
 }
 
-#define MAX_DATA_SIZE (7090 * 2) /* timed by the safty factor 2 */
+#define MAX_DATA_SIZE (7089 * MAX_STRUCTURED_SYMBOLS + 1) /* an upper bound (cf. the specification) */
 static unsigned char data_buffer[MAX_DATA_SIZE];
 static unsigned char *readFile(FILE *fp, int *length)
 {